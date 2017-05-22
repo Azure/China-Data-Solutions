20c978d6efc97c836a5e3b5ad458cdda0bd2c8f4
Yan Zhao <yazha@microsoft.com>
2017-05-22 17:04:27 +0800

    Add SNA Demo Pipleline (#44)
    * Initial Weibo SNA Pipeline
    
    Deploy Script
    
    Add Retry for KOL Analysis
    
    Add Logs for KOL Aanlysis
    
    * Add the diffusion prob training and predict file.
    fix an issue that VMName substring fail in deploy script.
    check the KOL model using page ranking.
    
    * update database script for adding table Weibo_icons and views
    
    * Remove unused App.config

---
+++ Media/SnaCN/src/SNASite/DataLibrary/Models/Diffusion_prob.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace DataLibrary.Models
+{
+    public partial class Diffusion_prob
+    {
+        public string uid { get; set; }
+        public Nullable<double> value { get; set; }
+    }
+}
