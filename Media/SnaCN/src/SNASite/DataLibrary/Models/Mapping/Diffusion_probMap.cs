02454ab8e19aab3de10717e550f68defaf7b869e
Yan Zhao <yazha@microsoft.com>
2017-05-24 20:43:49 +0800

    Modify the Diffusion_prob schema and import the file.

--- Media/SnaCN/src/SNASite/DataLibrary/Models/Mapping/Diffusion_probMap.cs
+++ Media/SnaCN/src/SNASite/DataLibrary/Models/Mapping/Diffusion_probMap.cs
@@ -8,17 +8,40 @@ namespace DataLibrary.Models.Mapping
         public Diffusion_probMap()
         {
             // Primary Key
-            this.HasKey(t => t.uid);
+            this.HasKey(t => new { t.user_followers_count, t.user_statuses_count, t.id });
 
             // Properties
-            this.Property(t => t.uid)
-                .IsRequired()
+            this.Property(t => t.kol_uid)
                 .HasMaxLength(50);
+
+            this.Property(t => t.user_followers_count)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            this.Property(t => t.user_statuses_count)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            this.Property(t => t.user_gender)
+                .HasMaxLength(50);
+
+            this.Property(t => t.user_province)
+                .HasMaxLength(50);
+
+            this.Property(t => t.user_verified)
+                .HasMaxLength(10);
+
+            this.Property(t => t.id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
 
             // Table & Column Mappings
             this.ToTable("Diffusion_prob");
-            this.Property(t => t.uid).HasColumnName("uid");
+            this.Property(t => t.kol_uid).HasColumnName("kol_uid");
+            this.Property(t => t.user_followers_count).HasColumnName("user_followers_count");
+            this.Property(t => t.user_statuses_count).HasColumnName("user_statuses_count");
+            this.Property(t => t.user_gender).HasColumnName("user_gender");
+            this.Property(t => t.user_province).HasColumnName("user_province");
+            this.Property(t => t.user_verified).HasColumnName("user_verified");
             this.Property(t => t.value).HasColumnName("value");
+            this.Property(t => t.id).HasColumnName("id");
         }
     }
 }
