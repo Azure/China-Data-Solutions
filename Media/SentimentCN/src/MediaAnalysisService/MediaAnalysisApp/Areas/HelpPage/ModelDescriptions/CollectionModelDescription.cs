c87e6fa50e2e02c79f4591d749de5e02e93a3be5
mswezhao <wezhao@microsoft.com>
2017-10-12 09:35:04 +0800

    update file name

--- Media/SentimentCN/src/MediaAnalysisService/MediaAnalysisApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MediaAnalysisApp.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
